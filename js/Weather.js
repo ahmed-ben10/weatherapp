c247295036d1455cfa1313a07770ccb618d36512
Ahmed ben <302267915@student.rocmondriaan.nl>
2020-03-13 13:16:38 +0100

    drag and drop working

--- js/Weather.js
+++ js/Weather.js
@@ -1,7 +1,6 @@
 class Weather {
 
     constructor() {
-        this.city = null;
         this.searchedWeather = null;
         this.localWeather = null;
     }
--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 class Main {
     constructor() {
+        this.drop = null;
+        this.dragover = null;
+        this.dragstart = null;
         this.load();
-        this.eventHandlers();
+        this.resume = true;
     }
 
     async load() {
@@ -10,74 +13,181 @@ class Main {
         this.searchedWeatherList = [];
         this.getFromLocalStorage();
         this.displayLocalWeather();
+        this.eventHandlers();
+    }
+
+    async eventHandlers() {
+        document.querySelector('.button-up').addEventListener('click', () => {
+            window.scrollTo({top: -10000, behavior: 'smooth'});
+        });
+        document.querySelector(".search").addEventListener('submit', async (e) => {
+            e.preventDefault();
+            const weather = new Weather();
+            const query = document.querySelector(".search__input").value;
+            const result = await weather.fetchSearchedData(query.trim());
+            switch (result.cod) {
+                case 200:
+                    this.saveToLocalStorage(result);
+                    this.displaySearchedWeather();
+                    window.scrollTo({top: document.body.scrollHeight + 100000, behavior: 'smooth'});
+                    document.querySelector(".search__input").value = "";
+                    document.querySelector(".search__error").innerHTML = "";
+                    break;
+                case "400":
+                    document.querySelector(".search__error").innerHTML = "Vult u alstublieft iets in";
+                    break;
+                case "404":
+                    document.querySelector(".search__error").innerHTML = "Helaas, de stad die u probeerde te zoeken is niet gevonden.";
+                    break;
+                default:
+                    document.querySelector(".search__error").innerHTML = "Sorry, Er is wat fout gegaan contacteer de beheerder.";
+                    break;
+            }
+        });
+
+        const buttons = await document.querySelectorAll('.weather__delete');
+        for (const button of buttons){
+            button.addEventListener('click', (e) =>{
+                this.removeWeather(button.dataset.id);
+            });
+        }
+    }
+
+    async dragable(){
+        const weatherDivs = await document.querySelectorAll('.searched-weather');
+        for (const weatherDiv of weatherDivs) {
+            weatherDiv.removeEventListener('drop', this.drop);
+            weatherDiv.removeEventListener('dragover', this.dragover);
+            weatherDiv.removeEventListener('dragstart', this.dragstart);
+        }
+
+        for (const weatherDiv of weatherDivs){
+            weatherDiv.addEventListener('drop', this.drop = (ev)=>{
+                event.preventDefault ();
+                let src = null;
+                for (const weatherDiv of weatherDivs) {
+                    if(ev.dataTransfer.getData("src") === weatherDiv.dataset.id){
+                        src  = weatherDiv
+                    }
+                }
+                var tgt = ev.currentTarget.firstElementChild;
+                var block = document.querySelector('.weather');
+                var nodes = Array.prototype.slice.call(tgt.parentNode.parentNode.children);
+                block.insertBefore(src,block.children[nodes.indexOf(tgt.parentNode)]);
+                block.insertBefore(block.children[nodes.indexOf(tgt.parentNode)],src);
+                console.log(src.dataset.id);
+                console.log(src,'src');
+                console.log(block,'block');
+                console.log(tgt.parentNode,'tgt');
+                console.log('-------------------');
+                this.swapPositions(src,tgt)
+
+            });
+            weatherDiv.addEventListener('dragover', this.dragover = (ev)=>{
+                ev.preventDefault ();
+            });
+            weatherDiv.addEventListener('dragstart', this.dragstart = (ev) =>{
+                ev.dataTransfer.setData ("src", ev.target.dataset.id);
+            });
+        }
+    }
+
+    swapPositions(src, tgt){
+        for (let i = tgt.dataset.id; i < this.searchedWeatherList.length; i++) {
+            if(i = src.dataset.id){
+                this.searchedWeatherList[tgt.dataset.id]
+            }
+        }
+        localStorage.setItem("searchedWeather", JSON.stringify(this.searchedWeatherList))
     }
 
     displayLocalWeather() {
+        let localWeather = this.localWeather.localWeather.list[0];
         document.querySelector(".local-weather__header").innerHTML = `
-            <h2 class="local-weather__heading">${this.localWeather.localWeather.list[0].name}</h2>
-            <img src=\"http://openweathermap.org/img/wn/${this.localWeather.localWeather.list[0].weather[0].icon}@2x.png\" alt=\"\"> 
-            <p>${this.localWeather.localWeather.list[0].weather[0].description}</p>
+            <h2 class="local-weather__heading">
+                ${localWeather.name} , ${localWeather.sys.country}
+            </h2>
+            <img src=\"http://openweathermap.org/img/wn/
+                ${
+                    localWeather.weather[0].icon
+                }@2x.png\" alt=\"\"
+            > 
+            <p>
+                ${localWeather.weather[0].description}
+            </p>
         `;
         document.querySelector(".local-weather__temp").innerHTML =
-            this.localWeather.localWeather.list[0].main.temp.toFixed(0) + "&#8451;";
-        document.querySelector(".local-weather__temp--estimated").innerHTML =
-            this.localWeather.localWeather.list[0].main.temp_min.toFixed(0)+ "&#8451; - " +
-            this.localWeather.localWeather.list[0].main.temp_max.toFixed(0)+ "&#8451;";
-
+            'Temp: ' + localWeather.main.temp.toFixed(0) + "&#8451;";
+        document.querySelector(".local-weather__temp--min").innerHTML =
+            'Min: ' + localWeather.main.temp_min.toFixed(0) + "&#8451;";
+        document.querySelector(".local-weather__temp--max").innerHTML =
+            'Max: ' + localWeather.main.temp_max.toFixed(0) + "&#8451;";
+        document.querySelector(".local-weather__details").innerHTML =
+            "<p>" +
+            " Windkracht: " + (localWeather.wind.speed * 1.609344).toFixed(0) + " km/h" +
+            "</p>" +
+            "<p> " +
+                "Gevoelstempratuur: " + localWeather.main.feels_like.toFixed(0) + "&#8451; " +
+            "</p>";
     }
+
     displaySearchedWeather() {
         let weatherSection = document.querySelector(".weather");
         weatherSection.innerHTML = "";
         for (let i = 0; i < this.searchedWeatherList.length; i++) {
             if (this.searchedWeatherList[i] == null) continue;
             let weatherBlock = `
-                <div class="searched-weather">
+                <div class="searched-weather" draggable="true" data-id="${this.searchedWeatherList[i].id}">
                     <div class="weather__header">
-                        <h2 class="weather__heading">${this.searchedWeatherList[i].name}</h2>
+                        <h2 class="weather__heading">${this.searchedWeatherList[i].name}, ${this.searchedWeatherList[i].sys.country}</h2>
                         <img src=\"http://openweathermap.org/img/wn/${this.searchedWeatherList[i].weather[0].icon}@2x.png\" alt=\"\"> 
-                        <p>${this.searchedWeatherList[i].weather[0].description}</p>
+                        <p>
+                            ${this.searchedWeatherList[i].weather[0].description}
+                        </p>
                     </div>
-                    <p class="weather__temp">${this.searchedWeatherList[i].main.temp.toFixed(0)}&#8451;</p>
-                    <p class="local-weather__temp--estimated">
-                        ${this.searchedWeatherList[i].main.temp_min.toFixed(0)}&#8451; 
-                        - ${this.searchedWeatherList[i].main.temp_max.toFixed(0)}&#8451;
+                    <p class="weather__temp"> Temp:
+                        ${this.searchedWeatherList[i].main.temp.toFixed(0)}&#8451;
                     </p>
-                    <button class="weather__delete" onclick="a.removeWeather(${this.searchedWeatherList[i].id});">DEL</button>
+                    <div class="weather__temp--estimated">
+                            <p class="weather__temp--min">
+                                Min: ${this.searchedWeatherList[i].main.temp_min.toFixed(0)}&#8451;
+                            </p>
+                            <p class="weather__temp--max">
+                                Max: ${this.searchedWeatherList[i].main.temp_max.toFixed(0)}&#8451;
+                            </p>
+                        </div>
+                    <button class="weather__delete" data-id="${this.searchedWeatherList[i].id}"><i class="fa fa-remove"></i></button>
+                    <div class="local-weather__details">
+                        <p> Windkracht: 
+                            ${(this.searchedWeatherList[i].wind.speed * 1.609344).toFixed(0)}
+                            km/h
+                        </p>
+                        <p> Gevoelstempratuur: 
+                            ${this.searchedWeatherList[i].main.feels_like.toFixed(0)}
+                        </p>                    
+                    </div>
                 </div>
             `;
             weatherSection.insertAdjacentHTML('beforeend', weatherBlock);
         }
-    }
-
-    removeWeather(id) {
-        for (let i = 0; i < this.searchedWeatherList.length; i++) {
-            if (this.searchedWeatherList[i] == null) continue;
-            else if (this.searchedWeatherList[i].id == id) {
-                this.searchedWeatherList.splice(i, 1);
-            }
-        }
-        this.saveToLocalStorage();
-        this.displaySearchedWeather();
+        this.dragable();
+        this.eventHandlers();
     }
 
     getFromLocalStorage() {
         if (JSON.parse(localStorage.getItem("searchedWeather"))) {
             this.searchedWeatherList = JSON.parse(localStorage.getItem("searchedWeather"));
-            console.log(this.searchedWeatherList);
             this.displaySearchedWeather();
         }
     }
 
     saveToLocalStorage(item) {
         let exist = false;
-        console.log(item);
         if (item) {
             this.searchedWeatherList.filter(weather => {
                 if (weather == null) return false;
                 if (weather.name === item.name) {
-                    console.log('duplicate');
                     exist = weather.name === item.name;
-                    console.log(weather.name === item.name)
                 }
             });
         }
@@ -87,28 +197,16 @@ class Main {
         }
     }
 
-
-    eventHandlers() {
-        document.querySelector(".search").addEventListener('submit', async (e) => {
-            e.preventDefault();
-            let weather = new Weather();
-            let query = document.querySelector(".search__input").value;
-            let result = await weather.fetchSearchedData(query.trim());
-            if (result.cod == 200) {
-                this.saveToLocalStorage(result);
-                this.displaySearchedWeather();
-                document.querySelector(".search__input").value = "";
+    removeWeather(id) {
+        for (let i = 0; i < this.searchedWeatherList.length; i++) {
+            if (this.searchedWeatherList[i] == null) continue;
+            if (this.searchedWeatherList[i].id == id) {
+                this.searchedWeatherList.splice(i, 1);
             }
-        });
-
-        // let weather = document.querySelector('.button__delete');
-        // for (var i = 0; i < weather.length; i++) {
-        //     weather[i].addEventListener('click', function(event) {
-        //         this.removeWeather()
-        //     });
-        // }
+        }
+        this.saveToLocalStorage();
+        this.displaySearchedWeather();
     }
-
 }
 
-let a = new Main();
+ new Main();
